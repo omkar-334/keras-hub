c510e4d2e74a9877879a8a4fee9b26bbf5dd972f
Omkar Kabde <omkarkabde@gmail.com>
2025-04-14 12:32:29 +0530

    add temp

--- keras_hub/src/utils/transformers/convert_debertav3.py
+++ keras_hub/src/utils/transformers/convert_debertav3.py
@@ -33,14 +33,23 @@ def convert_weights(backbone, loader, transformers_config):
         hf_weight_key="embeddings.LayerNorm.weight",
     )
 
+    # Hook functions
+
     def transpose_and_reshape(x, shape):
-        return np.reshape(np.transpose(x), shape)
+        return np.reshape(np.transpose(x, (1, 0)), shape)
+
+    def reshape(x, shape):
+        return np.array(x, dtype=np.float32).reshape(shape)
+
+    def transpose(x, _):
+        return np.transpose(x, axes=(1, 0))
 
     # Attention blocks
     for i in range(backbone.num_layers):
         block = backbone.get_layer(f"disentangled_attention_encoder_layer_{i}")
         attn = block._self_attention_layer
         hf_prefix = "encoder.layer."
+
         # Attention layers
         loader.port_weight(
             keras_variable=attn._query_dense.kernel,
@@ -48,14 +57,29 @@ def convert_weights(backbone, loader, transformers_config):
             hook_fn=transpose_and_reshape,
         )
         loader.port_weight(
+            keras_variable=attn._query_dense.bias,
+            hf_weight_key=f"{hf_prefix}{i}.attention.self.query_proj.bias",
+            hook_fn=reshape,
+        )
+        loader.port_weight(
             keras_variable=attn._key_dense.kernel,
             hf_weight_key=f"{hf_prefix}{i}.attention.self.key_proj.weight",
             hook_fn=transpose_and_reshape,
         )
         loader.port_weight(
+            keras_variable=attn._key_dense.bias,
+            hf_weight_key=f"{hf_prefix}{i}.attention.self.key_proj.bias",
+            hook_fn=reshape,
+        )
+        loader.port_weight(
             keras_variable=attn._value_dense.kernel,
             hf_weight_key=f"{hf_prefix}{i}.attention.self.value_proj.weight",
             hook_fn=transpose_and_reshape,
+        )
+        loader.port_weight(
+            keras_variable=attn._value_dense.bias,
+            hf_weight_key=f"{hf_prefix}{i}.attention.self.value_proj.bias",
+            hook_fn=reshape,
         )
         loader.port_weight(
             keras_variable=attn._output_dense.kernel,
@@ -65,7 +89,7 @@ def convert_weights(backbone, loader, transformers_config):
         loader.port_weight(
             keras_variable=attn._output_dense.bias,
             hf_weight_key=f"{hf_prefix}{i}.attention.output.dense.bias",
-            hook_fn=transpose_and_reshape,
+            hook_fn=reshape,
         )
         loader.port_weight(
             keras_variable=block._self_attention_layer_norm.beta,
@@ -75,27 +99,26 @@ def convert_weights(backbone, loader, transformers_config):
             keras_variable=block._self_attention_layer_norm.gamma,
             hf_weight_key=f"{hf_prefix}{i}.attention.output.LayerNorm.weight",
         )
-        # Intermediate layer
-        loader.port_weight(
-            keras_variable=block._feedforward_intermediate_dense.bias,
-            hf_weight_key=f"{hf_prefix}{i}.intermediate.dense.bias",
-            hook_fn=transpose_and_reshape,
-        )
+
+        # Intermediate layer (MLP)
         loader.port_weight(
             keras_variable=block._feedforward_intermediate_dense.kernel,
             hf_weight_key=f"{hf_prefix}{i}.intermediate.dense.weight",
-            hook_fn=transpose_and_reshape,
+            hook_fn=transpose,
+        )
+        loader.port_weight(
+            keras_variable=block._feedforward_intermediate_dense.bias,
+            hf_weight_key=f"{hf_prefix}{i}.intermediate.dense.bias",
         )
 
         loader.port_weight(
-            keras_variable=block._feedforward_output_dense.bias,
-            hf_weight_key=f"{hf_prefix}{i}.output.dense.bias",
-            hook_fn=transpose_and_reshape,
-        )
-        loader.port_weight(
             keras_variable=block._feedforward_output_dense.kernel,
             hf_weight_key=f"{hf_prefix}{i}.output.dense.weight",
-            hook_fn=transpose_and_reshape,
+            hook_fn=transpose,
+        )
+        loader.port_weight(
+            keras_variable=block._feedforward_output_dense.bias,
+            hf_weight_key=f"{hf_prefix}{i}.output.dense.bias",
         )
         loader.port_weight(
             keras_variable=block._feedforward_layer_norm.beta,
@@ -105,19 +128,20 @@ def convert_weights(backbone, loader, transformers_config):
             keras_variable=block._feedforward_layer_norm.gamma,
             hf_weight_key=f"{hf_prefix}{i}.output.LayerNorm.weight",
         )
-        # Relative Embeddings
-        loader.port_weight(
-            keras_variable=backbone.get_layer("relative_embeddings").rel_embeddings,
-            hf_weight_key=f"{hf_prefix}{i}.rel_embeddings.weight",
-        )
-        loader.port_weight(
-            keras_variable=backbone.get_layer("relative_embeddings").layer_norm.beta,
-            hf_weight_key=f"{hf_prefix}{i}.LayerNorm.bias",
-        )
-        loader.port_weight(
-            keras_variable=backbone.get_layer("relative_embeddings").layer_norm.gamma,
-            hf_weight_key=f"{hf_prefix}{i}.LayerNorm.weight",
-        )
+
+    # Relative Embeddings
+    loader.port_weight(
+        keras_variable=backbone.get_layer("rel_embedding").rel_embeddings,
+        hf_weight_key="encoder.rel_embeddings.weight",
+    )
+    loader.port_weight(
+        keras_variable=backbone.get_layer("rel_embedding").layer_norm.beta,
+        hf_weight_key="encoder.LayerNorm.bias",
+    )
+    loader.port_weight(
+        keras_variable=backbone.get_layer("rel_embedding").layer_norm.gamma,
+        hf_weight_key="encoder.LayerNorm.weight",
+    )
 
 
 def convert_tokenizer(cls, preset, **kwargs):
